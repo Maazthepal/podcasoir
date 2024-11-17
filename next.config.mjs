f711baa783e58a44ec4700ea5734ff4a22064f41
Maaz_Asif <124569696+Maazthepal@users.noreply.github.com>
2024-11-17 11:09:18 -0800

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wary-gopher-177.convex.cloud'
+      },
+
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'sleek-capybara-771.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'dusty-canary-352.convex.cloud'
+      }
+    ]
+  }
+};
+
+export default nextConfig;
